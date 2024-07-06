e8994f5bbf4471fa88cde5e1d4ea47e20723c926
kukuruzvelt <sergejkurnosenko@gmail.com>
2024-07-06 12:32:41 +0300

    feat(#1): update entities

--- src/Entity/Cart.php
+++ src/Entity/Cart.php
@@ -3,6 +3,9 @@
 namespace App\Entity;
 
 use App\Repository\CartRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\Criteria;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CartRepository::class)]
@@ -16,6 +19,17 @@ class Cart
     #[ORM\OneToOne(inversedBy: 'cart', cascade: ['persist', 'remove'])]
     #[ORM\JoinColumn(nullable: false)]
     private ?User $user = null;
+
+    /**
+     * @var Collection<int, CartProduct>
+     */
+    #[ORM\OneToMany(targetEntity: CartProduct::class, mappedBy: 'cart', orphanRemoval: true)]
+    private Collection $cartProducts;
+
+    public function __construct()
+    {
+        $this->cartProducts = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -32,5 +46,56 @@ class Cart
         $this->user = $user;
 
         return $this;
+    }
+
+    /**
+     * @return Collection<int, CartProduct>
+     */
+    public function getCartProducts(): Collection
+    {
+        return $this->cartProducts;
+    }
+
+    public function addProduct(Product $product, int $amount): static
+    {
+        $cartProduct = $this->getCartProduct($product);
+
+        if ($cartProduct === null) {
+            $cartProduct = $this->createCartProduct($product);
+            $this->cartProducts->add($cartProduct);
+        }
+
+        $cartProduct->setProductAmount($cartProduct->getProductAmount() + $amount);
+
+        return $this;
+    }
+
+    public function removeProduct(Product $product): static
+    {
+        $cartProduct = $this->getCartProduct($product);
+
+        if ($this->cartProducts->removeElement($cartProduct)) {
+            if ($cartProduct->getCart() === $this) {
+                $cartProduct->setCart(null);
+            }
+        }
+
+        return $this;
+    }
+
+    private function getCartProduct(Product $product): ?CartProduct
+    {
+        $criteria = Criteria::create()->where(Criteria::expr()->eq('product', $product));
+        $cartProducts = $this->cartProducts->matching($criteria);
+
+        return $cartProducts->isEmpty() ? null : $cartProducts->first();
+    }
+
+    private function createCartProduct(Product $product): CartProduct
+    {
+        return (new CartProduct())
+            ->setProduct($product)
+            ->setCart($this)
+            ->setProductAmount(0);
     }
 }
